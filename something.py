58cc211fbc3b584a7b0c3456276e2d04b1703b86
MadzhisE <78480012+MadzhisE@users.noreply.github.com>
2023-11-12 23:08:11 +0200

    Add files via upload

---
+++ something.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import sync_playwright
+from io import StringIO
+import xlsxwriter
+import pandas as pd
+
+PERSONAS_KODS = input("Ievadat savu personas kodu: ")
+PAROLE = input("Ievadat savu paroli: ")
+
+NERADIT_CHROME = True
+ATZIMES_LINK = "https://my.e-klase.lv/Family/ReportPupilMarks/Get"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=NERADIT_CHROME)
+    page = browser.new_page()
+    page.goto("https://www.e-klase.lv/")
+    persontextbox = page.locator("(//input[@placeholder='Lietotājvārds'])[2]")
+    passtextbox = page.locator("(//input[@placeholder='Parole'])[2]")
+    persontextbox.fill(PERSONAS_KODS)
+    passtextbox.fill(PAROLE)
+    page.locator("(//button[@type='submit'])[3]").click()
+    page.goto(ATZIMES_LINK)
+
+    workbook = xlsxwriter.Workbook("Atzimes.xlsx")
+    workbook.close()
+    
+    dfs = pd.read_html(StringIO(page.content()))
+    df = pd.concat(dfs)
+
+    writer = pd.ExcelWriter("Atzimes.xlsx", engine='xlsxwriter')
+    worksheet = writer.book.add_worksheet('Sheet1')
+
+    for idx, col in enumerate(df):  # loop through all columns
+        series = df[col]
+        max_len = max((
+        series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+
+    df.to_excel(writer, index=False)
+
+    writer.close()
+
+    browser.close()
